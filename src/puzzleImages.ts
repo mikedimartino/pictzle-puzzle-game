0937d30aa225def489749561c866c580292c68b3
Mike Dimartino <mikedimartino@yahoo.com>
2023-03-24 10:14:06 -0700

    Add more images

--- src/puzzleImages.ts
+++ src/puzzleImages.ts
@@ -2,10 +2,14 @@ import BeachStreet from './assets/images/beach-street.jpg';
 import BuffaloGrazing from './assets/images/buffalo-grazing.jpg';
 import CaliforniaCoast from './assets/images/california-coast.jpg';
 import Clownfish from './assets/images/clownfish.jpg';
+import DeerFamily from './assets/images/deer-family.jpg';
 import HollywoodPalms from './assets/images/hollywood-palms.jpg';
+import HouseInterior from './assets/images/house-interior.jpg';
 import Panda from './assets/images/panda.jpg';
+import Penguins from './assets/images/penguins.jpg';
 import SnowyMountainLake from './assets/images/snowy-mountain-lake.jpg';
 import VolkswagenBug from './assets/images/vw-bug.jpg';
+import WhaleJumping from './assets/images/whale-jumping.jpg';
 import randomize from './utils/randomize';
 
 export const imageList = randomize([
@@ -17,6 +21,10 @@ export const imageList = randomize([
   Panda,
   SnowyMountainLake,
   VolkswagenBug,
+  Penguins,
+  WhaleJumping,
+  HouseInterior,
+  DeerFamily,
 ]);
 
 export default imageList;
